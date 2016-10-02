aa0fac48bf5a91a98cc8b9927b1ee5d3141ab2e3
Jurien Meerlo <jurienmeerlo@live.nl>
2016-10-02 01:04:20 +0100

    added text background to setup the scaling properly. scaling works now. it scales 4x up. design_width = 160, design_height = 144. end result = width 640, height 576.

--- LuxeGBJam5/src/Main.hx
+++ LuxeGBJam5/src/Main.hx
@@ -1,17 +1,28 @@
 
+import luxe.Camera.SizeMode;
 import luxe.GameConfig;
 import luxe.Input;
+import luxe.Sprite;
+import luxe.Vector;
+
+import phoenix.Texture;
 
 class Main extends luxe.Game 
 {
+    private var design_width:Int = 160;
+    private var design_height:Int = 144;
+
+    var zoom:Float;
 
     override function config (config:GameConfig) 
     {
 
-        config.window.title = 'luxe game';
-        config.window.width = 960;
-        config.window.height = 640;
+        config.window.title = 'GB Jam 5 Entry';
+        config.window.width = 640;
+        config.window.height = 576;
         config.window.fullscreen = false;
+
+        config.preload.textures.push({ id: 'assets/images/background.png' });
 
         return config;
 
@@ -19,7 +30,28 @@ class Main extends luxe.Game
 
     override function ready () 
     {
+        // get the zoom factor for the screen size
+        zoom = Math.floor(Luxe.screen.h / design_height);
 
+        // get the new camera size
+        var screen_w:Int = Math.floor(Luxe.screen.w / zoom);
+        var screen_h:Int = Math.floor(Luxe.screen.h / zoom);
+
+        // set the camera size
+        Luxe.camera.size = new Vector(screen_w, screen_h);
+        Luxe.camera.size_mode = SizeMode.fit;
+
+        // load the image and set the image filter to nearest neighbor to help the pixel art
+        var image:Texture = Luxe.resources.texture('assets/images/background.png');
+        image.filter_min = image.filter_mag = FilterType.nearest;
+
+        // add the sprite to the screen
+        new Sprite({
+            name: 'background',
+            texture: image,
+            size: new Vector(design_width, design_height),
+            centered: false
+        });
     } // ready
 
     override function onkeyup (event:KeyEvent) 
